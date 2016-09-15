182e88507827bff1e06bb70345384cca75fbfad4
Miguel De Jesus <miggydj@gmail.com>
2016-09-15 17:04:37 +0800

    env example and inventory modified

--- resources/views/pages/inventory.blade.php
+++ resources/views/pages/inventory.blade.php
@@ -1,70 +1,79 @@
-@extends('main')
+<!DOCTYPE html>
+<html>
 
-@section('title', '| All Assets')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Elevated Play Asset Management System">
+    <meta name="author" content="Miguel de Jesus | Joanne de Vera">
 
-@section('content')
+    <title>EPAMS | Inventory Page</title>
 
+    {{ Html::style('css/bootstrap.min.css')}}
+    {{ Html::style('css/styles.css')}}
+
+</head>
+
+<body>
     <div class="row">
-      <div class="col-lg-10">
+      <div class="col-md-10 col-md-offset-1">
         <h1 class="page-header">
           Inventory
         </h1>
       </div>
     </div>
     <div class="row">
-      <ol class="breadcrumb">
-          <li>
-              <i class="fa fa-dashboard"></i>  <a href="/dashboard">Dashboard |</a>
-              <i class="fa fa-table"></i>  <a href="/inventory">Inventory</a>
-          </li>
-      </ol>
-    </div>
-    <hr>
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default table-responsive">
+            <table class="table table-sm table-condensed table-bordered">
+              <thead>
+                <th>ID</th>
+                <th>Device Name</th>
+                <th>Model</th>
+                <th>Resolution</th>
+                <th>Processor</th>
+                <th>RAM</th>
+                <th>OS</th>
+                <th>GPU</th>
+                <th>x32/x64</th>
+                <th>Category</th>
+                <th>Sim Support</th>
+              </thead>
 
-    <div class="panel panel-default table-responsive">
+              <tbody>
+                @foreach($assets as $asset)
 
-        <table class="table table-sm table-condensed table-bordered">
-          <thead>
-            <th>ID</th>
-            <th>Device Name</th>
-            <th>Model</th>
-            <th>Resolution</th>
-            <th>Processor</th>
-            <th>RAM</th>
-            <th>OS</th>
-            <th>GPU</th>
-            <th>x32/x64</th>
-            <th>Category</th>
-            <th>Sim Support</th>
-          </thead>
+                  <tr>
+                      <th>{{ $asset->id }}</th>
+                      <td>{{ $asset->name}}</td>
+                      <td>{{ $asset->model}}</td>
+                      <td>{{ $asset->resolution}}</td>
+                      <td>{{ $asset->processor}}</td>
+                      <td>{{ $asset->ram}}</td>
+                      <td>{{ $asset->os}}</td>
+                      <td>{{ $asset->gpu}}</td>
+                      <td>{{ $asset->bit}}</td>
+                      <td>{{ $asset->category}}</td>
+                      <td>{{ $asset->simSupport}}</td>
+                  </tr>
 
-          <tbody>
-            @foreach($assets as $asset)
+                @endforeach
+              </tbody>
+            </table>
 
-              <tr>
-                  <th>{{ $asset->id }}</th>
-                  <td>{{ $asset->name}}</td>
-                  <td>{{ $asset->model}}</td>
-                  <td>{{ $asset->resolution}}</td>
-                  <td>{{ $asset->processor}}</td>
-                  <td>{{ $asset->ram}}</td>
-                  <td>{{ $asset->os}}</td>
-                  <td>{{ $asset->gpu}}</td>
-                  <td>{{ $asset->bit}}</td>
-                  <td>{{ $asset->category}}</td>
-                  <td>{{ $asset->simSupport}}</td>
-              </tr>
+            <!-- pagination for laravel from paginate() method-->
+            <div class="text-center">
+              {!! $assets->links(); !!}
+            </div>
 
-            @endforeach
-          </tbody>
-        </table>
-
-        <!-- pagination for laravel from paginate() method-->
-        <div class="text-center">
-          {!! $assets->links(); !!}
         </div>
-
-
+      </div>
     </div>
 
-@endsection
+
+    @include('partials._javascript')
+
+</body>
+
+</html>
